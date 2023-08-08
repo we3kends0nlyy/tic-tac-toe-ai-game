219b1029be7033a622076e868c7438968e6236be
Julian Aparicio <japaric4@uci.edu>
2023-08-07 23:40:25 -0700

    Updated the game state to recognize different conditions of the game(winner, game over, a tie, game not started). This makes it easier for the computer to know what stage of the game it is in.

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -2,6 +2,17 @@ import enum
 from dataclasses import dataclass
 from functools import cached_property
 
+WINNING_PATTERNS = (
+    "???......",
+    "...???...",
+    "......???",
+    "?..?..?..",
+    ".?..?..?.",
+    "..?..?..?",
+    "?...?...?",
+    "..?.?.?..",
+)
+
 class Mark(enum.StrEnum):
     CROSS = "X"
     NAUGHT = "O"
@@ -29,3 +40,46 @@ class Grid:
     @cached_property
     def empty_count(self) -> int:
         return self.cells.count(" ")
+    
+@dataclass(frozen=True)
+class Move:
+    mark: Mark
+    cell_index: int
+    before_state: "GameState"
+    after_state: "GameState"
+
+@dataclass(frozen=True)
+class GameState:
+    grid: Grid
+    starting_mark: Mark = Mark("X")
+
+    @cached_property
+    def current_mark(self) -> Mark:
+        if self.grid.x_count == self.grid.o_count:
+            return self.starting_mark
+        else:
+            return self.starting_mark.other
+    
+    @cached_property
+    def game_not_started(self) -> bool:
+        return self.grid.empty_count == 9
+    
+    @cached_property
+    def game_over(self) -> bool:
+        return self.winner is not None or self.tie
+    
+    @cached_property
+    def tie(self) -> bool:
+        return self.winner is None and self.grid.empty_count == 0
+    
+    @cached_property
+    def winning_cells(self) -> list[int]:
+        for pattern in WINNING_PATTERNS:
+            for mark in Mark:
+                if re.match(pattern.replace("?", mark), self.grid.cells):
+                    return [
+                        match.start()
+                        for match in re.finditer(r"\?", pattern)
+
+                    ]
+        return []
