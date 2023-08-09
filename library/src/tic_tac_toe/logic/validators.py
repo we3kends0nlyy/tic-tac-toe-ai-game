e37e3016b5788d440c2ca8aab6cf909c512689af
Julian Aparicio <japaric4@uci.edu>
2023-08-09 16:53:35 -0700

    I added some logic to the engine to make sure that players cannot choose the same mark. The engine connects with the renderer to make sure that the game advances each time a player makes a move. The player objects are also connected to the X and O marks to make sure that each player is distinguished in game.

--- library/src/tic_tac_toe/logic/validators.py
+++ library/src/tic_tac_toe/logic/validators.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from tic_tac_toe.game.players import Player
     from tic_tac_toe.logic.models import GameState, Grid, Mark
 
 import re
@@ -45,3 +46,6 @@ def validate_winner(grid: Grid, starting_mark: Mark, winner: Mark | None) -> Non
             if grid.o_count != grid.x_count:
                 raise InvalidGameState("Wrong number of Os")
             
+def validate_players(player1: Player, player2: Player) -> None:
+    if player1.mark is player2.mark:
+        raise ValueError("Players must use different marks")
